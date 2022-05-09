2f389e87c50fe8f640df9c6d35bdc8c3978facaa
philip martinez <martinez.philip00@gmail.com>
2022-05-08 22:05:24 -0500

    commit tree e58c0832e2dba0bf5ba81d4ef03c67342fd9137b

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.splice(-1);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.splice(0,1);
+}
+
+function appendCat(Broom) {
+    const newCats = [...cats, 'Broom'];
+    return newCats
+}
+
+function prependCat (Arnold) {
+    const newCats = ['Arnold', ...cats];
+    return newCats
+}
+
+function removeLastCat() {
+    const newCats = cats.slice();
+    newCats.pop('Garfield');
+    return newCats
+}
+
+function removeFirstCat () {
+    const newCats = cats.slice();
+    newCats.shift('Milo');
+    return newCats
+}
